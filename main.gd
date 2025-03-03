d3d73df29277fdaf8f5ccb782c81a8b977e60727
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2025-03-03 20:52:53 +0100

    raycast stinky poo poo blä äckel

--- main.gd
+++ main.gd
@@ -8,18 +8,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $Player.velocity is Vector3:
-		var velocity = $Player.velocity
-		var facing = $Player.facing
+			$DebugLabel.text = "pos: %v\nvelocity: %v accel_dir: %v in: %v\nfacing: %v h: %v\neurot: %v h: %v" % [
+			$Player.position, $Player.velocity, $Player.accel_direction, $Player.input_direction, 
+			$Player.facing, $Player.facingh, $Player/Pivot.rotation, $Player/HorizontalPivot.rotation]
 
-		$VelocityLabel.text = "Velocity: %s\n / %v\n
-								facing: %v, %s %s\n
-								basis: %v h: %v" % [velocity.length(), velocity, facing, $Player.angleh, $Player.anglev, $Player/Pivot.rotation, $Player/HorizontalPivot.rotation]
-
-		var facingh = Vector2(facing.x, facing.z)*10
-		var velocityh = Vector2(velocity.x, velocity.z)*20
-		var vangle = velocityh.angle_to(facingh)
-
-		$FacingVisualizer.target_position = facingh
-		$VelocityVisualizer.target_position = Vector2(cos(vangle), sin(vangle)) * velocityh.length()
-	else:
-		pass
+			var angle = $Player.velocityh2d.angle_to($Player.facingh2d)
+			$RelativeVelocityVisualizer.target_position = Vector2(0, -1).rotated(angle)*$Player.speed
--- player.gd
+++ player.gd
@@ -1,19 +1,32 @@
 extends CharacterBody3D
 
+var speed = 0
+
 var accel = 50
 var fric = 4
 var air_accel = 50
 var air_fric = 0.01
 
+var accel_direction := Vector3(0, 0, 0)
+var input_direction := Vector2(0, 0)
+
 var mouse_sens = 0.4
+
+var space: PhysicsDirectSpaceState3D
+
 var angleh := 0.0
 var anglev := 0.0
-
-var facing = Vector3(0, 0, 0)
-
-var speed = 0
+var facing := Vector3(0, 0, 0)
+var facingh := Vector3(0, 0, 0)
+var facingh2d := Vector2(0, 0)
+var velocityh := Vector3(0, 0, 0)
+var velocityh2d := Vector2(0, 0)
 
 @onready var explosion_preload = preload("res://explosion.tscn")
+
+func cast_ray_from_camera():
+	var query = PhysicsRayQueryParameters3D.create(global_position, )
+	return space.intersect_ray(query)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -29,10 +42,6 @@ func _input(event: InputEvent) -> void:
 			anglev += changev
 		else:
 			anglev = roundf(anglev)
-		
-		$HorizontalPivot.global_transform.basis = Basis.from_euler(Vector3(0, deg_to_rad(angleh), 0))
-
-		$Pivot.global_transform.basis = Basis.from_euler(Vector3(deg_to_rad(anglev), deg_to_rad(angleh), 0))
 	
 	if event.is_action_pressed("jump") and is_on_floor():
 		velocity += Vector3(0, 5, 0)
@@ -42,29 +51,27 @@ func _input(event: InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
+
 	if event.is_action_pressed("shoot"):
-		var ray := RayCast3D.new(); add_child(ray)
-		ray.target_position = facing*1000
-
-		print("shooting at: " + str(ray.target_position))
-
-		if ray.is_colliding():
-			var collision := ray.get_collision_point()
-			print("shoot collision: " + str(collision))
-
-			var explosion: Node3D = explosion_preload.instantiate()
-			get_tree().root.add_child(explosion)
-
-			explosion.global_position = collision
+		$Pivot/Camera.
 
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = $HorizontalPivot.rotation * Vector3(input_dir.x, 0, input_dir.y)
+	space = get_world_3d().direct_space_state
 
-	facing = Vector3(sin(deg_to_rad(-angleh)), deg_to_rad(anglev), cos(deg_to_rad(angleh)))
+	$HorizontalPivot.global_transform.basis = Basis.from_euler(Vector3(0, deg_to_rad(angleh), 0))
+	$Pivot.global_transform.basis = Basis.from_euler(Vector3(deg_to_rad(anglev), deg_to_rad(angleh), 0))
+
+	facing = Vector3(-sin(deg_to_rad(angleh)), deg_to_rad(anglev), -cos(deg_to_rad(angleh)))
+	facingh = Vector3(-sin(deg_to_rad(angleh)), 0, -cos(deg_to_rad(angleh)))
+	facingh2d = Vector2(facingh.x, facingh.z)
+
+	velocityh = Vector3(velocity.x, 0, velocity.z)
+	velocityh2d = Vector2(velocity.x, velocity.z)
 
 	speed = velocity.length()
+
+	input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	accel_direction = $HorizontalPivot.basis * Vector3(input_direction.x, 0, input_direction.y)
 
 	if is_on_floor():
 		# friction
@@ -73,12 +80,12 @@ func _physics_process(delta: float) -> void:
 			velocity *= max(speed - drop, 0) / speed
 
 		# accel
-		velocity += direction * accel * delta
+		velocity += accel_direction * accel * delta
 	else:
 		# grav
 		velocity.y += get_gravity().y * delta 
 
 		# accel
-		velocity += direction * air_accel * delta
+		velocity += accel_direction * air_accel * delta
 
 	move_and_slide()
